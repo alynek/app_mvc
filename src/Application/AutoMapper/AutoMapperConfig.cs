fd6b540ff65c973de9b5b63f959be572de967da7
Alyne Gois <alyne0x67@gmail.com>
2021-09-19 15:42:13 -0300

    Cria as ViewModels das entidades, configura o AutoMapper na classe Startup

---
+++ src/Application/AutoMapper/AutoMapperConfig.cs
@@ -0,0 +1,16 @@
+﻿using Application.ViewModels;
+using AutoMapper;
+using Business.Models;
+
+namespace Application.AutoMapper
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<Fornecedor, FornecedorViewModel>().ReverseMap();
+            CreateMap<Endereco, EnderecoViewModel>().ReverseMap();
+            CreateMap<Produto, ProdutoViewModel>().ReverseMap();
+        }
+    }
+}
--- src/Application/Startup.cs
+++ src/Application/Startup.cs
@@ -1,4 +1,5 @@
 using Application.Data;
+using AutoMapper;
 using Business.Interfaces;
 using Data.Context;
 using Data.Repository;
@@ -24,15 +25,13 @@ namespace Application
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseMySql(
-                    Configuration.GetConnectionString("DefaultConnection")));
-
             services.AddDbContext<AppDbContext>(options =>
                 options.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddAutoMapper(typeof(Startup));
 
             services.AddControllersWithViews();
 
