3232b364238acd441f82aa69eee1b4fa7990077a
Alyne Gois <alyne0x67@gmail.com>
2021-10-22 18:53:59 -0300

    Cria interfaces e Adiciona Validações com o pacote FluentValidation

---
+++ src/Business/Services/ForncedorService.cs
@@ -0,0 +1,32 @@
+﻿using Business.Interfaces;
+using Business.Models;
+using Business.Models.Validations;
+using System;
+using System.Threading.Tasks;
+
+namespace Business.Services
+{
+    public class ForncedorService : BaseService, IFornecedorService
+    {
+        public async Task Adicionar(Fornecedor fornecedor)
+        {
+            if (!ExecutarValidacao(new FornecedorValidation(), fornecedor)
+                && !ExecutarValidacao(new EnderecoValidation(), fornecedor.Endereco)) return;
+        }
+
+        public async Task Atualizar(Fornecedor fornecedor)
+        {
+            if (!ExecutarValidacao(new FornecedorValidation(), fornecedor)) return;
+        }
+
+        public async Task AtualizarEndereco(Endereco endereco)
+        {
+            if (!ExecutarValidacao(new EnderecoValidation(), endereco)) return;
+        }
+
+        public Task Remover(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Business/Services/ProdutoService.cs
@@ -0,0 +1,26 @@
+﻿using Business.Interfaces;
+using Business.Models;
+using Business.Models.Validations;
+using System;
+using System.Threading.Tasks;
+
+namespace Business.Services
+{
+    public class ProdutoService : BaseService, IProdutoService
+    {
+        public async Task Adicionar(Produto produto)
+        {
+            if (!ExecutarValidacao(new ProdutoValidation(), produto)) return;
+        }
+
+        public async Task Atualizar(Produto produto)
+        {
+            if (!ExecutarValidacao(new ProdutoValidation(), produto)) return;
+        }
+
+        public Task Remover(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
